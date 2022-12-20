c8b3dcdc2254c227060ace073a3cd3a6338314f3
Graham Waterfield <you@example.com>
2022-12-20 20:02:12 +0000

    Preparing for Dynamic Content

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
+
+def open_connection():
+    connection = g.getattr('_connection', None)
+    if connection is None:
+        connection = sqlite3.connect(PATH)
+        g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+def execute_sql():
+    pass
 
 @app.route('/')
 @app.route('/jobs')
